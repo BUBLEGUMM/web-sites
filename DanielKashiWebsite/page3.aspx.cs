b0b852b59bc7740eda87fa87685db09d29521f94
OriZarmany <orizar16@gmail.com>
2025-12-02 16:49:57 +0200

    addedmmultiplication table

--- DanielKashiWebsite/page3.aspx.cs
+++ DanielKashiWebsite/page3.aspx.cs
@@ -7,8 +7,21 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    public string strkefel;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        strkefel += "<table border='1'>";
+        for (int i = 1; i <= 10; i++)
+        {
+            strkefel += "<tr>";
+            for (int j = 1; j <= 10; j++)
+            {
+                strkefel += "<td>" + (i * j) + "</td>";
+            }
+            strkefel += "</tr>";
+        }
+        strkefel += "</table>";
     }
 }
